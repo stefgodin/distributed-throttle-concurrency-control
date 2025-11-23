ef76af026a55fc16db8fb967e8d1c6968770be8c
stephanegodin <stephanegodin@outlook.com>
2025-11-22 20:16:48 -0500

    Add project files.

---
+++ concurrency-control/Program.cs
@@ -0,0 +1,96 @@
+﻿using concurrency_control;
+using Microsoft.Data.Sqlite;
+using System.Security.Cryptography;
+
+class Program
+{
+    const string DSN = "Data Source=concurrency.db;";
+
+    static void Main()
+    {
+        Setup();
+        RunSimulation().Wait();
+    }
+
+    static void Setup()
+    {
+        if (!File.Exists("concurrency.db"))
+        {
+            using (var connection = new SqliteConnection(DSN))
+            {
+                connection.Open();
+
+                var createTableCommand = connection.CreateCommand();
+                createTableCommand.CommandText =
+                """
+                    CREATE TABLE IF NOT EXISTS RequestQueue (
+                        Id INTEGER PRIMARY KEY,
+                        ActivationTime INTEGER,
+                        TimeoutTime INTEGER NOT NULL
+                    );
+                """;
+                createTableCommand.ExecuteNonQuery();
+            }
+        }
+    }
+
+    static async Task RunSimulation()
+    {
+        using (var connection = new SqliteConnection(DSN))
+        {
+            connection.Open();
+            using (var walModeCmd = connection.CreateCommand())
+            {
+                walModeCmd.CommandText = "PRAGMA journal_mode = WAL;";
+                walModeCmd.ExecuteNonQuery();
+            }
+
+            Console.WriteLine("Simulating requests (1 at a time per execution)");
+            Console.WriteLine();
+            while (true)
+            {
+                var rq = new RequestQueue(connection, maxConcurrentRequests: 5);
+                await SimulateRequest(rq);
+                await Task.Delay(RandomNumberGenerator.GetInt32(1500));
+                Console.WriteLine();
+            }
+        }
+    }
+
+    static async Task SimulateRequest(RequestQueue rq)
+    {
+        try
+        {
+            PriorityLevel priority = RandomNumberGenerator.GetInt32(3) switch
+            {
+                1 => PriorityLevel.HIGH,
+                2 => PriorityLevel.MEDIUM,
+                _ => PriorityLevel.LOW,
+            };
+
+            Console.WriteLine($"Initiating task with priority {Enum.GetName(typeof(PriorityLevel), priority)}");
+            var start = DateTime.Now;
+
+            var ttl = 5;
+            var slot = await rq.ReserveSlot(priority, ttl);
+            Console.WriteLine($"Task reserved with priority {Enum.GetName(typeof(PriorityLevel), priority)} after {(DateTime.Now - start).TotalMilliseconds}ms");
+            if (RandomNumberGenerator.GetInt32(500) == 0)
+            { // 0.2% chance to fail silently after reservation (ex: job timeout, server restart, etc.)
+                Console.WriteLine($"Task failed after reservation (1 in 500 chance)");
+                return;
+            }
+
+            var processingTime = 10 + RandomNumberGenerator.GetInt32(4500);
+            Console.WriteLine($"Faking Processing time of {processingTime}ms");
+            await Task.Delay(processingTime);
+            Console.WriteLine($"Task done with priority {Enum.GetName(typeof(PriorityLevel), priority)} after {(DateTime.Now - start).TotalMilliseconds}ms");
+
+            await rq.FreeSlot(slot);
+            Console.WriteLine($"Task freed");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ concurrency-control/RequestQueue.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Security.Cryptography;
+
+namespace concurrency_control
+{
+    class RequestQueue
+    {
+        private const int TIMEOUT = 60; // Wait TIMEOUT seconds considering data stale
+
+        protected SqliteConnection connection;
+        protected int maxConcurrentRequests;
+        public RequestQueue(SqliteConnection connection, int maxConcurrentRequests)
+        {
+            this.connection = connection;
+            this.maxConcurrentRequests = maxConcurrentRequests;
+        }
+
+        public async Task<Int64> ReserveSlot(PriorityLevel priority, int ttl)
+        {
+            var maxReservedSlot = GetPriorityMaxReservedSlot(priority);
+
+            using var registerCmd = connection.CreateCommand();
+            registerCmd.CommandText = "INSERT INTO RequestQueue (Id, TimeoutTime) VALUES (@id, unixepoch('now') + " + TIMEOUT + ");";
+            registerCmd.Parameters.AddWithValue("id", 0);
+
+            using var reserveCmd = connection.CreateCommand();
+            reserveCmd.CommandText = "UPDATE RequestQueue SET ActivationTime = unixepoch('now'), TimeoutTime = unixepoch('now') + @ttl WHERE id = @id AND (SELECT COUNT(*) < " + maxReservedSlot + " FROM RequestQueue WHERE ActivationTime IS NOT NULL AND TimeoutTime > unixepoch('now'))";
+            reserveCmd.Parameters.AddWithValue("ttl", ttl);
+            reserveCmd.Parameters.AddWithValue("id", 0);
+            Int64 id = 0;
+            DateTime creationTime = DateTime.MinValue;
+            bool reserved = false;
+            do
+            {
+                if (creationTime.AddSeconds(TIMEOUT) < DateTime.Now || id == 0)
+                {
+                    id = RandomNumberGenerator.GetInt32(Int32.MaxValue);
+                    creationTime = DateTime.Now;
+                    registerCmd.Parameters["id"].Value = id;
+                    registerCmd.ExecuteNonQuery();
+
+                    reserveCmd.Parameters["id"].Value = id;
+                }
+
+                reserved = reserveCmd.ExecuteNonQuery() >= 1;
+                if (!reserved)
+                {
+                    await Task.Delay(50);
+                }
+            }
+            while (!reserved);
+
+            return id;
+        }
+
+        private int GetPriorityMaxReservedSlot(PriorityLevel priority)
+        {
+            return priority switch
+            {
+                PriorityLevel.HIGH => maxConcurrentRequests,
+                PriorityLevel.MEDIUM => Math.Max(1, Convert.ToInt32(maxConcurrentRequests * 0.8)),
+                _ => Math.Max(1, Convert.ToInt32(maxConcurrentRequests * 0.6)),
+            };
+        }
+
+        public async Task FreeSlot(Int64 id)
+        {
+            using var freeCmd = connection.CreateCommand();
+            freeCmd.CommandText = "DELETE FROM RequestQueue WHERE id = @id OR TimeoutTime <= unixepoch('now');"; // Cleans up old slots at the same time
+            freeCmd.Parameters.AddWithValue("id", id);
+            freeCmd.ExecuteNonQuery();
+        }
+    }
+
+    enum PriorityLevel
+    {
+        HIGH = 0,
+        MEDIUM = 1,
+        LOW = 2,
+    }
+}
